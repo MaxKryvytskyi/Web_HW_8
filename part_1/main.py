f582553d7077f6ad08c8e0fa0b2b676b33294225
Max <maxkryvytskyiwork@gmail.com>
2024-01-24 05:26:06 +0200

    save

--- part_1/main.py
+++ part_1/main.py
@@ -1,18 +1,25 @@
-import part_1.connect_db
 
-
-
+from connect_db import connect
+from models import Quotes, Authors
+from pprint import pprint
 
 def main():
     while True:
-        case = input("---> ")
-        match case:
-            case "name":
-                pass
-            case "tag":
-                pass
-            case "tags":
-                pass
+        cases = input("---> ").split(" ")
+
+        name = " ".join(cases[1::])
+        match cases[0]:
+            case "name:":
+                author = Authors.objects(fullname=name).first()
+                quotess = Quotes.objects.filter(author=author.id)
+                print(len(quotess))
+                for qoutes in quotess:
+                    pprint(f"{qoutes['tags']} {name} {qoutes['quote']}")
+                print("name:")
+            case "tag:":
+                print("tag:")
+            case "tags:":
+                print("tags:")
             case "exit":
                 break
 
